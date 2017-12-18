e9ae16cb5bb12e859d37c67db7627e58e73c1bd7
Horidas Roy <horidasr@gmail.com>
2017-12-18 11:29:13 +0600

    insert-get operation is done paritally

--- mongoDb/routes/index.js
+++ mongoDb/routes/index.js
@@ -1,17 +1,56 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',condition:false, anyArray: [1, 3, 2] });
-});
-router.get('/test/:id', function(req, res, next){
-  res.render('test', {output: req.params.id});
+  res.render('index');
 });
 
-
-router.post('/test/submit', function(req, res, next){
-  var id = req.body.nothing;
-  res.redirect('/test/' + id);
+router.get('/get-data', function(req, req, next){
+  var resultArray = [];
+  mongo.connect(url, function(err, db){
+    assert.equal(null, err);
+    var cursor = db.collection('user-data').find();
+    cursor.forEach(function(doc, err){
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function(){
+      db.close();
+      res.render('index', {items: resultArray});
+    });
+  });
 });
+
+router.post('/insert', function(req, res, next){
+
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+
+  res.redirect('/');
+
+  mongo.connect(url, function(err, db){
+    assert.equal(null, err);
+    db.collection('user-data').insertOne(item,function(err, resulr){
+      assert.equal(null, err);
+      console.log('Data inserted');
+      db.close();
+    });
+  });
+});
+
+router.post('/update', function(req, res, next){
+
+});
+
+router.post('/delete', function(req, res, next){
+
+});
+
 module.exports = router;
